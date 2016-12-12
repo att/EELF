aeb7a156e53e3d1fbcd6a43d6bde899cb511b854
sv8675 <sv8675@users.noreply.github.com>
2016-12-12 13:54:01 -0600

    Update TestLogger.java

--- EELF/src/test/java/com/att/eelf/TestLogger.java
+++ EELF/src/test/java/com/att/eelf/TestLogger.java
@@ -37,7 +37,7 @@ public class TestLogger {
 	 @Before
 	 public void setUpMDC() {
 		 MDC.clear();
-		 MDC.put(MDC_KEY_REQUEST_ID, "cc6965");
+		 MDC.put(MDC_KEY_REQUEST_ID, "cc69");
 		 
 		 String hostIpAddress = "";
 		 String hostName = "";
